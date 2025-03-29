ecaaed601c7ab5cf45de44f644115b9cd579ce36
Im-fast-as-blitz <rodion.chernomordin@icloud.com>
2025-03-29 19:31:52 +0300

    make baseline for train

--- train.py
+++ train.py
@@ -2,10 +2,11 @@ import gymnasium as gym
 from logger import TensorboardSummaries as Summaries
 import torch
 import numpy as np
-from tqdm import tqdm
+from tqdm import trange
+import os
 
 
-from models import SAC
+from utils import parse_args, get_model
 
 
 seed = 13
@@ -16,27 +17,32 @@ torch.manual_seed(seed);
 def main():
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-    env = gym.make()
-    env = Summaries(env, ...);
+    args = parse_args()
 
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0] 
+    env = gym.make(args.task, render_mode="rgb_array", include_cfrc_ext_in_observation=False)
+    env = Summaries(env, args.model + " on " + args.task);
 
-    gamma=0.99                    # discount factor
-    max_buffer_size = 10**5       # size of experience replay
-    start_timesteps = 5000        # size of experience replay when start training
-    timesteps_per_epoch=1         # steps in environment per step of network updates
-    batch_size=128                # batch size for all optimizations
-    max_grad_norm=10              # max grad norm for all optimizations
-    tau=0.005                     # speed of updating target networks
-    policy_update_freq=1          # frequency of actor update; vanilla choice is 1 for SAC
-    alpha=0.1                     # temperature for SAC
+    global_params = {}
+    global_params["state_dim"] = env.observation_space.shape[0]
+    global_params["action_dim"] = env.action_space.shape[0]
+    global_params["device"] = DEVICE
 
-    model = ....to(DEVICE)
+    timesteps_per_epoch=1
+
+    model = get_model(args.model, global_params)
 
     # start train
-    for n_iterations in tqdm(range(0, 1000000, timesteps_per_epoch)):
-        loss = model.run_iter(env=env)
+    for _ in trange(0, args.nepochs, timesteps_per_epoch):
+        model.run_iter(env=env)
+
+
+    # save actor
+    dir_path = "save"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    torch.save({
+        'model_state_dict': model.get_actor().state_dict(),
+    }, f'{dir_path}/model_{args.model}_checkpoint.pth')
 
 
 if __name__ == "__main__":
---
+++ utils/arg_parser.py
@@ -0,0 +1,34 @@
+import argparse
+
+from models import SAC
+
+import json
+
+def get_model_config(model_name):
+    try:
+        with open(f'{model_name}.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print("Файл не найден!")
+    except json.JSONDecodeError:
+        print("Ошибка декодирования JSON!")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+def get_model(model_name, global_params):
+    model_params = get_model_config(model_name) | global_params
+    if model_name == "SAC":
+        return SAC(**model_params)
+    else:
+        raise "Неизвестный алгоритм :("
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--task', type=str, required=True)
+    parser.add_argument('-m', '--model', type=str, required=True)
+
+
+    return parser.parse_args()
